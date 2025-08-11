392fce0e4e7c33fb44f8aece74d0e23d53d2e11c
admin <scarandry@gmail.com>
2025-08-11 05:40:05 +0200

    fixed linting errors

--- .next/static/chunks/app/cardetails/page-aecf060a8b8593da.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[410],{},function(n){n.O(0,[972,569,482,971,117,744],function(){return n(n.s=2482)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/cars/[slug]/page-f78ae6a023595d70.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[466],{},function(n){n.O(0,[569,972,482,971,117,744],function(){return n(n.s=2482)}),_N_E=n.O()}]);
