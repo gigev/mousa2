898ec0915dcdb0d716a4e6066cf45a81ac9ee123
avassv <avassv@asd.com>
2025-08-15 09:31:48 +0400

    last update phone+comp buttons fix centered

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -221,8 +221,8 @@ const CarouselPrevious = React.forwardRef<
       className={cn(
         "absolute h-8 w-8 rounded-full z-10",
         orientation === "horizontal"
-          ? "-left-2 top-1/2 -translate-y-1/2"
-          : "-top-2 left-1/2 -translate-x-1/2 rotate-90",
+          ? "left-2 top-1/2 -translate-y-1/2"
+          : "top-2 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
       disabled={!canScrollPrev}
@@ -253,8 +253,8 @@ const CarouselNext = React.forwardRef<
       className={cn(
         "absolute h-8 w-8 rounded-full z-10",
         orientation === "horizontal"
-          ? "-right-2 top-1/2 -translate-y-1/2"
-          : "-bottom-2 left-1/2 -translate-x-1/2 rotate-90",
+          ? "right-2 top-1/2 -translate-y-1/2"
+          : "bottom-2 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
       disabled={!canScrollNext}
